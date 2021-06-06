53cce02b4d529449a8e73c650a8a54eb7053b4f4
himanshu-tiw <82417065+himanshu-tiw@users.noreply.github.com>
2021-06-06 14:03:10 +0530

    Update Footer.jsx

--- Footer.jsx
+++ Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   var current_Year = new Date().getFullYear();
   return (
     <footer>
-      <p>Copyrigth @ {current_Year}</p>
+      <p>Copyright @ {current_Year}</p>
     </footer>
   );
 }
